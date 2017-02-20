14e7c8f200ce1f13c72394d5748f70fe29c1bb0e
Jordan Myers <myersjo@tcd.ie>
2017-02-20 19:32:34 +0000

    Updated file sources in repository index.php

--- code/repository/index.php
+++ code/repository/index.php
@@ -1,19 +1,12 @@
 <?php
   session_start();
-  if (isset($_SESSION['userlogin'])) {
+  if (isset($_SESSION['userlogin'], $_SESSION['userfirstname'])) {
     $email = $_SESSION['userlogin'];
-    unset($_SESSION['canvas_id']);
-    require_once('../../php/db_utils.php');
-    $conn = db_connect(); // Connect to the database
-
-    // Check if the username exists
-    if(($result = mysqli_query($conn, "SELECT name FROM user WHERE username = '$email'"))) {
-      // Check that there is only one user with this email
-      if(mysqli_num_rows($result) == 1) {
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $name = $row['name'];
-        $_SESSION['userfirstname'] = $name;
-      }
+    $name = $_SESSION['userfirstname'];
+    if (isset($_SESSION['canvas_id'])) {
+      $canvas_id = $_SESSION['canvas_id'];
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -24,17 +17,19 @@
         <title>The Ethics Canvas</title>
 
         <!-- favicons -->
-        <link rel="icon" href="../../icon/favicon.ico"/>
+        <link rel="icon" href="../icon/favicon.ico"/>
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <!-- Google font -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
         <!-- App style -->
-        <link rel="stylesheet" type="text/css" href="../css/dashboard.css">
-        <!-- retieving the user's email sent by php, as a js variable               -->
-        <script language="javascript" type="text/javascript">
-          var loggedin_user_email = '<?php echo $email; ?>';
+        <link rel="stylesheet" type="text/css" href="css/repository.css">
+        <!-- php variables have to be retieved here as js variables -->
+        <script type="text/javascript">
+          var email_save_canvas = '<?php echo $email; ?>';
+          var user_save_canvas = '<?php echo $name; ?>';
+          var current_canvas_id = '<?php echo $canvas_id; ?>';
         </script>
     </head>
 
@@ -105,15 +100,15 @@
               </div>
                <div class="license-icons">
                  <ul>
-                   <li><img src="icon/by.large.png" alt="ethics-canvas-by-icon"/> </li>
-                      <li><img src="icon/share.large.png" alt="ethics-canvas-share-icon"/> </li>
-                   <li><img src="icon/cc.large.png" alt="ethics-canvas-cc-icon"/> </li>
-                   <li> <img src="icon/remix.large.png" alt="ethics-canvas-remix-icon"/></li>
-                   <li> <img src="icon/sa.large.png" alt="ethics-canvas-sa-icon"/></li>
+                   <li><img src="../icon/by.large.png" alt="ethics-canvas-by-icon"/> </li>
+                      <li><img src="../icon/share.large.png" alt="ethics-canvas-share-icon"/> </li>
+                   <li><img src="../icon/cc.large.png" alt="ethics-canvas-cc-icon"/> </li>
+                   <li> <img src="../icon/remix.large.png" alt="ethics-canvas-remix-icon"/></li>
+                   <li> <img src="../icon/sa.large.png" alt="ethics-canvas-sa-icon"/></li>
                  </ul>
                </div>
-                <p class="ethics-copy terms"><a href="privacy-terms/terms.html">Terms of Service</a></p>
-                <p class="ethics-copy privacy"><a href="privacy-terms/privacy.html">Privacy Policy</a></p>
+                <p class="ethics-copy terms"><a href="../privacy-terms/terms.html">Terms of Service</a></p>
+                <p class="ethics-copy privacy"><a href="../privacy-terms/privacy.html">Privacy Policy</a></p>
 
                 <p class="ethics-copy">The ADAPT Centre for Digital Content Technology</p> <p class="ethics-copy">is funded under the SFI Research Centres Programme (Grant 13/RC/2106).</p><p class="ethics-copy">It is co-funded under the European Regional Development Fund.</p>
                 <p class="ethics-copy">Ethics Canvas v1.7|&copy; ADAPT Center & Trinity College Dublin & Dublin City University, 2016</p>
@@ -121,36 +116,14 @@
         </div>
       </div> <!-- end of outer container-fluid -->
 
-      <!-- add jquery -->
-      <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
-      <!-- jquery UI  -->
-      <script src="https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js" integrity="sha256-55Jz3pBCF8z9jBO1qQ7cIf0L+neuPTD1u7Ytzrp2dqo=" crossorigin="anonymous"></script>
-
-      <!-- Mailchimp Script-->
-      <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-      <script type="text/javascript">
-          (function($) {
-              window.fnames = new Array();
-              window.ftypes = new Array();
-              fnames[0] = 'EMAIL';
-              ftypes[0] = 'email';
-              fnames[1] = 'FNAME';
-              ftypes[1] = 'text';
-              fnames[2] = 'LNAME';
-              ftypes[2] = 'text';
-          }(jQuery));
-          var $mcj = jQuery.noConflict(true);
-      </script>
-
       <!-- jQuery -->
       <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+      <!-- jquery UI  -->
+      <script src="https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.min.js" integrity="sha256-55Jz3pBCF8z9jBO1qQ7cIf0L+neuPTD1u7Ytzrp2dqo=" crossorigin="anonymous"></script>
       <!-- Bootstrap -->
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
-      <!-- The Slick Carousel jQuery plugin (slider) -->
-      <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
       <!-- The app javascript -->
-      <script src="js/landing.js" charset="utf-8"></script>
+      <script src="js/repository.js" charset="utf-8"></script>
   </body>
 
 </html>
