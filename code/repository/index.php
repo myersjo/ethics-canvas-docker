7515ecb8d25181e1eb2de07a9c4f74a4b4d7219d
Jordan Myers <myersjo@tcd.ie>
2017-02-20 20:46:30 +0000

    Updated css for repo homepage and added content

--- code/repository/index.php
+++ code/repository/index.php
@@ -86,7 +86,9 @@
               </h1>
           </div>
           <div class="col-md-12">
-            Search for a canvas or browse the featured canvases below.
+            <p>
+              Search for a canvas or browse the featured canvases below.
+            </p>
           </div>
         </div>
 
@@ -103,6 +105,9 @@
         </div><!-- /.row -->
 
         <div class="row">
+          <div class="col-lg-12">
+            <h2>Featured Canvases</h2>
+          </div>
           <div class="col-md-4">
               <div class="panel panel-default">
                   <div class="panel-heading">
