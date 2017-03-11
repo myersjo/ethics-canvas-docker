d4dc42ea490c8b6f0bcb2c6256da57aaa3923513
Jordan Myers <myersjo@tcd.ie>
2017-03-11 20:43:03 +0000

    Added outline of function for search box submission

---
+++ code/repository/js/repository.js
@@ -0,0 +1,24 @@
+$(function() {
+
+  /*======================
+    Search box
+  ======================*/
+  $('.search-box').on('submit', function(event) {
+    var url = "../php/search.php";
+    var query = $('#search-box-input').val();
+    $.getJSON(url, query, function(returnedData) {
+      var canvases = [];
+      var resultHTML = '';
+
+      // an array of canvas_id's is returned
+      $.each(returnedData, function(index, value) {
+        var canvas = {};
+        canvas.name; // from canvas table
+        canvas.username; // from canvas table
+        canvas.tags[]; // from tag_relation table
+        canvases.push(canvas);
+      }) //end of $.each(returnedData)
+
+    }); // end of $.getJSON
+  }); // end of $('.search-box').on('submit' ...
+});
