b9e7e99fe76463634e04f12633d2935fac6d87e6
Jordan Myers <myersjo@lg12l4.scss.tcd.ie>
2017-03-14 11:47:46 +0000

    Updated search.php

--- code/repository/php/search.php
+++ code/repository/php/search.php
@@ -33,9 +33,12 @@ $canvases;
          )
        );
        if (($tags = mysqli_query($conn, "SELECT tags.tag_name as tag_name FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tag_relation.canvas_id=’$canvas_id’"))) {
+         $tagsArr > array();
          while ($tagRow = mysqli_fetch_assoc(tags)) {
-         	$canvases[$canvas_id]["tags"] = $tagRow["tag_name"];
+           $tagsArr[] = $tagRow["tag_name"];
+         	
          }
+         $canvases[$canvas_id]["tags"] = $tagsArr;
        }
      }
      echo json_encode($canvases);
