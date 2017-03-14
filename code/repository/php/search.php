6dad57176fd5fd754b07e49fb8db093065049951
Jordan Myers <myersjo@tcd.ie>
2017-03-14 17:23:38 +0000

    Stopped the same canvas being added twice in search.php. Happened when search query matched both the name and tag for a canvas.

--- code/repository/php/search.php
+++ code/repository/php/search.php
@@ -53,7 +53,6 @@
         }
         mysqli_free_result($tags);
       }
-      echo json_encode($canvases);
     }
 
     /***************************
@@ -67,31 +66,34 @@
     else {      // get details from canvas table
       while ($row = mysqli_fetch_assoc($result)) {
         $canvas_id = $row["canvas_id"];
-        $canvases[$canvas_id] = array(
-          "canvas_name" => $row["canvas_name"],
-          "user_id" => $row["user_id"],
-          "canvas_date" => $row["canvas_date"]
-        );
+        if(!array_key_exists($canvas_id, $canvases)) {
+          $canvases[$canvas_id] = array(
+            "canvas_name" => $row["canvas_name"],
+            "user_id" => $row["user_id"],
+            "canvas_date" => $row["canvas_date"]
+          );
+        }
       }
       mysqli_free_result($result);
       // get tags for each canvas from tag_relation table
       foreach($canvases as $canvas_id => $canvas) {
-        if (($tags = mysqli_query($conn, "SELECT tags.tag_name as tag_name FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tag_relation.canvas_id='$canvas_id'"))) {
-          // echo ' here ; ';
-          $tagsArr = array();
-          while ($tagRow = mysqli_fetch_assoc($tags)) {
-            array_push($tagsArr, $tagRow["tag_name"]);
+        if(count($canvas[tags]) <= 0) {
+          if (($tags = mysqli_query($conn, "SELECT tags.tag_name as tag_name FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tag_relation.canvas_id='$canvas_id'"))) {
+            // echo ' here ; ';
+            $tagsArr = array();
+            while ($tagRow = mysqli_fetch_assoc($tags)) {
+              array_push($tagsArr, $tagRow["tag_name"]);
+            }
+            // $canvases[$canvas_id]["tags"] = mysqli_fetch_array($tags);
+            $canvases[$canvas_id]["tags"] = $tagsArr;
           }
-          // $canvases[$canvas_id]["tags"] = mysqli_fetch_array($tags);
-          $canvases[$canvas_id]["tags"] = $tagsArr;
-        }
-        else {
-          echo 400; // Query error
+          else {
+            echo 400; // Query error
+          }
         }
         mysqli_free_result($tags);
       }
-      echo json_encode($canvases);
     }
-
+    echo json_encode($canvases);
     db_close($conn); // Close the database
   ?>
