460b146db5e223cc3ffe102af332bfd4646fb2c6
Jordan Myers <myersjo@lg12l4.scss.tcd.ie>
2017-03-14 11:34:08 +0000

    Updated search.php

--- code/repository/php/search.php
+++ code/repository/php/search.php
@@ -25,7 +25,7 @@ $canvases;
    else {
      while ($row = mysqli_fetch_assoc($result)) {
        $canvas_id = $row["canvas_id"];
-       $canvases['$canvas_id'] = array(
+       $canvases[$canvas_id] = array(
          "canvas_name" => $row["canvas_name"],
          "user_id" => $row["user_id"],
          "canvas_date" => $row["canvas_date"],
@@ -34,8 +34,7 @@ $canvases;
        );
        if (($tags = mysqli_query($conn, "SELECT tags.tag_name as tag_name FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tag_relation.canvas_id=’$canvas_id’"))) {
          while ($tagRow = mysqli_fetch_assoc(tags)) {
-           printf("%s", $tagRow['tag_name'] );
-         	$canvases['$canvas_id']["tags"] = array(
+         	$canvases[$canvas_id]["tags"] = array(
             $tagRow["tag_name"]
           );
          }
