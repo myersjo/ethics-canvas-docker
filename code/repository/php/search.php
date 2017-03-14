d2859b1679f275ef7c23a30cee24792727ce9627
Jordan Myers <myersjo@tcd.ie>
2017-03-14 17:14:51 +0000

    Test queries and search by tag. Added test queries in dbQueries.sql. Added 'search canvases by tag' in search.php.

--- code/repository/php/search.php
+++ code/repository/php/search.php
@@ -18,23 +18,26 @@
     );
   */
   $canvases;
-    // Search canvases by name
+
+    /***************************
+    * Search canvases by name *
+    ****************************
+    */
     if(!($result = mysqli_query($conn, "SELECT * FROM canvas WHERE canvas_name LIKE \"%$query%\""))) {
       echo 400; // Wrong query
     }
     else {
-      // $canvases = mysqli_fetch_all($result);
+      // get details from canvas table
       while ($row = mysqli_fetch_assoc($result)) {
         $canvas_id = $row["canvas_id"];
         $canvases[$canvas_id] = array(
           "canvas_name" => $row["canvas_name"],
           "user_id" => $row["user_id"],
-          "canvas_date" => $row["canvas_date"],
-          // "tags" => array(
-          // )
+          "canvas_date" => $row["canvas_date"]
         );
       }
       mysqli_free_result($result);
+      // get tags for each canvas from tag_relation table
       foreach($canvases as $canvas_id => $canvas) {
         if (($tags = mysqli_query($conn, "SELECT tags.tag_name as tag_name FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tag_relation.canvas_id='$canvas_id'"))) {
           // echo ' here ; ';
@@ -45,34 +48,50 @@
           // $canvases[$canvas_id]["tags"] = mysqli_fetch_array($tags);
           $canvases[$canvas_id]["tags"] = $tagsArr;
         }
-        else { 
-          // echo ' else here ; ';
-          echo mysqli_error($conn);
+        else {
+          echo 400; // Query error
         }
         mysqli_free_result($tags);
       }
       echo json_encode($canvases);
     }
 
-    // Search canvases by tags
-  /*   if(!($result = mysqli_query($conn, "SELECT * FROM tags WHERE tag_name LIKE '%$query%'"))) {
+    /***************************
+    * Search canvases by tag   *
+    ****************************
+    */
+    $sql = "SELECT DISTINCT tag_relation.canvas_id as canvas_id FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tags.tag_name LIKE '%est%'";
+     if(!($result = mysqli_query($conn, $sql))) {
         echo 400; // Wrong query
     }
-    else {
-        while ($row = mysqli_fetch_assoc(result)) {
-            $canvas_id = $row[canvas_id];
-            $canvases[$canvas_id] = array(
-                "canvas_name" => $row[canvas_name],
-                "user_id" => $row[user_id],
-                "canvas_date" => $row[canvas_date],
-                "tags" => array(
-                )
-            );
-            if ($tags = mysqli_query($conn, "SELECT * FROM tag_relation WHERE canvas_id='$canvas_id'")) {
-
-            }
+    else {      // get details from canvas table
+      while ($row = mysqli_fetch_assoc($result)) {
+        $canvas_id = $row["canvas_id"];
+        $canvases[$canvas_id] = array(
+          "canvas_name" => $row["canvas_name"],
+          "user_id" => $row["user_id"],
+          "canvas_date" => $row["canvas_date"]
+        );
       }
-  }
-  */
+      mysqli_free_result($result);
+      // get tags for each canvas from tag_relation table
+      foreach($canvases as $canvas_id => $canvas) {
+        if (($tags = mysqli_query($conn, "SELECT tags.tag_name as tag_name FROM tag_relation INNER JOIN tags ON tag_relation.tag_id=tags.id WHERE tag_relation.canvas_id='$canvas_id'"))) {
+          // echo ' here ; ';
+          $tagsArr = array();
+          while ($tagRow = mysqli_fetch_assoc($tags)) {
+            array_push($tagsArr, $tagRow["tag_name"]);
+          }
+          // $canvases[$canvas_id]["tags"] = mysqli_fetch_array($tags);
+          $canvases[$canvas_id]["tags"] = $tagsArr;
+        }
+        else {
+          echo 400; // Query error
+        }
+        mysqli_free_result($tags);
+      }
+      echo json_encode($canvases);
+    }
+
     db_close($conn); // Close the database
   ?>
