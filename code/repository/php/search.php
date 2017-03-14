5fc7e38d2a62934926d6fbf042487750975b2b32
Jordan Myers <myersjo@lg12l4.scss.tcd.ie>
2017-03-14 11:07:19 +0000

    Updated search.php

--- code/repository/php/search.php
+++ code/repository/php/search.php
@@ -22,14 +22,9 @@ $canvases;
    if(!($result = mysqli_query($conn, "SELECT * FROM canvas WHERE canvas_name LIKE \"%$query%\""))) {
      echo 400; // Wrong query
    }
-   elseif(mysqli_num_rows($result) == 0) {
-     echo 'No results ; ';
-   }
    else {
-     echo mysqli_num_rows($result);
      while ($row = mysqli_fetch_assoc($result)) {
        $canvas_id = $row[canvas_id];
-       echo "canvas_id: $canvas_id ; canvases json: ";
        $canvases[$canvas_id] = array(
          "canvas_name" => $row[canvas_name],
          "user_id" => $row[user_id],
@@ -37,8 +32,9 @@ $canvases;
          "tags" => array(
          )
        );
-       if ($tags = mysqli_query($conn, "SELECT * FROM tag_relation WHERE canvas_id='$canvas_id'")) {
+       if (($tags = mysqli_query($conn, "SELECT * FROM tag_relation WHERE canvas_id='$canvas_id'")) {
          while ($tagRow = mysqli_fetch_assoc(tags)) {
+           print_r($tagRow);
          	$canvases[$canvas_id][tags] = array(
             $tagRow[tag_name]
           );
@@ -68,7 +64,6 @@ $canvases;
      }
 }
 */
-  echo 'finished';
    mysqli_free_result($result);
    db_close($conn); // Close the database
 ?>
