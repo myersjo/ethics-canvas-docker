87fe16a9b15581923bba6cdc67b5a6e57a100f75
Jordan Myers <myersjo@tcd.ie>
2017-03-25 16:07:30 +0000

    Added jquery UI css

--- code/repository/index.php
+++ code/repository/index.php
@@ -21,6 +21,8 @@
 
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <!-- jQuery UI -->
+        <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
         <!-- Font Awesome -->
         <script src="https://use.fontawesome.com/f05ce90ed7.js"></script>
         <!-- Google font -->
