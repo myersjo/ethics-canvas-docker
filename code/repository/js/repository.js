9de51689cd9d0d4626c05addb60fb9a9dbf616af
Jordan Myers <myersjo@tcd.ie>
2017-03-26 20:08:57 +0100

    Updated tags in search results

--- code/repository/js/repository.js
+++ code/repository/js/repository.js
@@ -16,7 +16,7 @@ $(function() {
       $.each(canvases, function(key, value) {
         resultHTML += '<div class="col-md-4"><div class="panel panel-default"><div class="panel-heading"><h4><i class="fa fa-fw fa-th"></i>' + value["canvas_name"] + '</h4></div><div class="panel-body"><h5>Tags:</h5><p class="text-center">';
         $.each(value["tags"], function(i, tag) {
-          resultHTML += ' ' + tag + ' ';
+          resultHTML += '\t' + tag + '\t';
         });
         resultHTML += '</p><a href="#" class="btn btn-default">View</a></div></div></div>';
       }) //end of $.each(returnedData)
