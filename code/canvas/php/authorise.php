0c3c393b808c9ca4136d08f5612fbe66f7882737
Jordan Myers <myersjo@tcd.ie>
2017-04-04 15:40:11 +0100

    Added debug code to authorise.php

--- code/canvas/php/authorise.php
+++ code/canvas/php/authorise.php
@@ -34,8 +34,9 @@ if (isset($_GET['current_canvas_id'])) {
                 echo 401;
             }
             else {
+                echo ' here ';
                 $users = mysqli_fetch_all($usersRes);
-                echo 'users';
+                echo ' users ';
                 foreach($users as $user) {
                     if ($user['user_id'] == $user_id) {
                         echo 200;
